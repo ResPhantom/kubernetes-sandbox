49fbea279474e260c1bc18dab983cf5ca57ac979
Laptop mortal <mortal@rowan>
2023-05-20 10:16:28 +0200

    commented out some init

--- Hardware-VM/init.sh
+++ Hardware-VM/init.sh
@@ -12,53 +12,53 @@ apk update && apk upgrade
 apk update --available
 apk upgrade --available
 
-# Install util packages
-apk add cni-plugin-flannel \
-        cni-plugins \
-        flannel \
-        flannel-contrib-cni \
-        uuidgen \
-        nfs-utils
+# # Install util packages
+# apk add cni-plugin-flannel \
+#         cni-plugins \
+#         flannel \
+#         flannel-contrib-cni \
+#         uuidgen \
+#         nfs-utils
 
-# Install Kubernetes packages
-apk add kubelet \
-        kubeadm \
-        kubectl \
-        containerd
+# # Install Kubernetes packages
+# apk add kubelet \
+#         kubeadm \
+#         kubectl \
+#         containerd
 
-# Add kernel module for networking
-echo "br_netfilter" > /etc/modules-load.d/k8s.conf
-modprobe br_netfilter
-echo 1 > /proc/sys/net/ipv4/ip_forward
+# # Add kernel module for networking
+# echo "br_netfilter" > /etc/modules-load.d/k8s.conf
+# modprobe br_netfilter
+# echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Remove swap storage
-cat /etc/fstab | grep -v swap > temp.fstab
-cat temp.fstab > /etc/fstab
-rm temp.fstab
-swapoff -a
+# # Remove swap storage
+# cat /etc/fstab | grep -v swap > temp.fstab
+# cat temp.fstab > /etc/fstab
+# rm temp.fstab
+# swapoff -a
 
-# Fix id error messages
-uuidgen > /etc/machine-id
+# # Fix id error messages
+# uuidgen > /etc/machine-id
 
-# Add services
-rc-update add containerd
-rc-update add kubelet
-rc-update add ntpd
+# # Add services
+# rc-update add containerd
+# rc-update add kubelet
+# rc-update add ntpd
 
-# Start services
-/etc/init.d/ntpd start
-/etc/init.d/containerd start
+# # Start services
+# /etc/init.d/ntpd start
+# /etc/init.d/containerd start
 
-# Create flannel Symlink
-ln -s /usr/libexec/cni/flannel-amd64 /usr/libexec/cni/flannel
+# # Create flannel Symlink
+# ln -s /usr/libexec/cni/flannel-amd64 /usr/libexec/cni/flannel
 
-# Ensure that brigeded packets traverse iptable rules
-echo "net.bridge.bridge-nf-call-iptables=1" >> /etc/sysctl.conf
-sysctl net.bridge.bridge-nf-call-iptables=1
+# # Ensure that brigeded packets traverse iptable rules
+# echo "net.bridge.bridge-nf-call-iptables=1" >> /etc/sysctl.conf
+# sysctl net.bridge.bridge-nf-call-iptables=1
 
-# Pin your versions!  If you update and the nodes get out of sync, it implodes.
-apk add 'kubelet=~1.26'
-apk add 'kubeadm=~1.26'
-apk add 'kubectl=~1.26'
+# # Pin your versions!  If you update and the nodes get out of sync, it implodes.
+# apk add 'kubelet=~1.26'
+# apk add 'kubeadm=~1.26'
+# apk add 'kubectl=~1.26'
 
-# Note that in the future you will manually have to add a newer version the same way to upgrade.
+# # Note that in the future you will manually have to add a newer version the same way to upgrade.
